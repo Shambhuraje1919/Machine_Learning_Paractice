2186e1b59edeb8c7c8691f2958005aa0e27c000f
Shambhuraje Sunil Jagadale <shambhurajejagadale@gmail.com>
2025-12-01 16:40:33 +0530

    new Streamlit app devloped and learned new things about this libraires

---
+++ package/streamlit/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("First web page using Streamlit")
+st.subheader("This is the sub-header")
+st.text("This is the text")
+
+st.markdown("### $E = MC^2$")
+st.button("Click Me")
+
+find_your_LPA = st.selectbox(
+    "Software Engineer",
+    ["Amazon 22 LPA", "Google 30 LPA", "Microsoft 25 LPA"]
+)
+
+st.write("You selected:", find_your_LPA)
