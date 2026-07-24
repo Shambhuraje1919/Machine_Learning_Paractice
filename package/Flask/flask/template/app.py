322aea05e1b20f101c46f17ba2e8465ede70d958
Shambhuraje Sunil Jagadale <shambhurajejagadale@gmail.com>
2026-07-24 11:27:13 +0530

    Fix imports and typo in Flask app.py

--- package/Flask/flask/template/app.py
+++ package/Flask/flask/template/app.py
@@ -1,5 +1,5 @@
 ## Learning Flask - A Micro Web Framework for Python
-from flask import Flask
+from flask import Flask, request, render_template
 
 '''
 It creates an instance of class Flask, which acts as the WSGI application.
@@ -10,7 +10,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to Flask!"
+    return "<html><h1>Welcome to the Flask web page with HTML5</h1></html>"
+
+
+@app.route("/index", methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"Hello, {name}!"
+    return render_template('form.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
